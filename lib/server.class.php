6abdd29c31359a8bdca281ce666389326603be38
anythink <xi@anythink.com.cn>
2015-08-06 12:56:19 +0800

    no message

--- lib/server.class.php
+++ lib/server.class.php
@@ -5,15 +5,33 @@
  * Date: 15-8-3
  * Time: 下午6:32
  */
+
+
+class keyValue {
+	public static function __callStatic($name, $arguments)	{
+		if(empty($arguments)){
+			return self::shareGet($name);
+		}else{
+			return self::sharePut($name,$arguments[0]);
+		}
+	}
+
+	static function sharePut($k,$v){
+		$file = ROOT.'/data/'.$k;
+		file_put_contents($file,serialize($v));
+	}
+
+	static function shareGet($k){
+		$file = ROOT.'/data/'.$k;
+		return unserialize(file_get_contents($file));
+	}
+}
+
 class Server {
 
-	protected $online  = 0;                // 在线用户数
+	public $online = 0; // 在线用户数
 	protected $playerFlag = -1;             //播放服务状态
 
-	/**
-	 * @var swoole_websocket_server
-	 */
-	private $server;
 	/**
 	 * player 实例
 	 * @var player
@@ -38,84 +56,125 @@ class Server {
 	 */
 	public $playTime = 0;
 
-	function __construct(){
+
+	private $server;
+
+
+	function init(){
 		$server = new swoole_websocket_server("0.0.0.0", 8810);
 		$server->on('open', [$this, 'onOpen']);
 		$server->on('message', [$this, 'onMessage']);
 		$server->on('close', [$this, 'onClose']);
+		$GLOBALS['params']['online'] = 0;
 
 		$this->server = $server;
 		$this->player = new player();
 		$this->event  = new event();
+		keyValue::online(0);
+		keyValue::user([]);
 	}
 
 
 
 	function onOpen(swoole_websocket_server $_server, swoole_http_request $request)	{
-		if ($this->online == 0 && $this->playerFlag == -1) {
 
+		if (keyValue::online() == 0 && $this->playerFlag == -1) {
 			$this->playerFlag = swoole_timer_tick(1000,[$this,'onPlay']);
-			echo '播放模块已启动,模块ID：'.$this->playerFlag.PHP_EOL;
+			$this->serverLog('播放模块已启动,模块ID：'.$this->playerFlag);
 		}
-		$this->online++;
-		$this->event->onOpen();
-		echo "新用户连线:{$_server->worker_pid}: 标示 fd#{$request->fd} 在线:{$this->online}\n";
+
+		keyValue::online(keyValue::online()+1);
+		$user = keyValue::user();
+		$user[] = $request->fd;
+		keyValue::user($user);
+		$this->event->eventOpen($_server,$request);
+		$this->serverLog("新用户连线:{$_server->worker_pid}: 标示 fd#{$request->fd} 在线:".keyValue::online());
 	}
 
+
+
+
 	function onMessage(swoole_websocket_server $_server, $frame){
-
-		//$url = "/{$this->play}/time/{$this->muiscNow}";
-
-		echo "received " . strlen($frame->data) . " bytes\n";
-		//echo "receive from {$fd}:{$data},opcode:{$opcode},fin:{$fin}";
-		//$_server->push($frame->fd, "this is server");
-		//$_server->push($frame->fd, $url);
-		//$_server->close($frame->fd);
+		$frame->playId = $this->playId;
+		$frame->playTime = $this->playTime;
+		$this->event->eventMessage($_server,$frame);
+		//echo "received " . strlen($frame->data) . $this->playId .'<->'. $this->playTime." bytes\n";
+	}
+	static function badge($action,$message){
+		return json_encode(['act'=>$action,'data'=>$message]);
+	}
+	static function badgeDecode($str){
+		 json_decode($str);
+		$std = new stdClass();
+		$std->act = $str['act'];
+		$std->data = $str['data'];
+		return $std;
 	}
 
 	function onClose(swoole_websocket_server $_server, $fd){
-		$this->online--;
+		//$GLOBALS['params']['online'] -=1;
 
-		if ($this->online <= 0 && $this->playerFlag != -1) {
+		if ($GLOBALS['params']['online'] <= 0 && $this->playerFlag != -1) {
 			swoole_timer_clear($this->playerFlag);
-			echo '播放模块已停止工作'.PHP_EOL;
+			$this->serverLog("播放模块已停止工作");
 			$this->playerFlag = -1;
 			$this->playId=0;
 		}
-
-		echo "用户离线:{$_server->worker_pid}: 在线:{$this->online}\n";
+		$this->serverLog("用户离线:{$_server->worker_pid}: 在线:".$GLOBALS['params']['online']);
 	}
 
 	function onPlay(){
 
 		if($this->playId == 0) {
-			echo '正在获取播放列表信息'.PHP_EOL;
+			$this->serverLog("server.class->onplay 正在取播放列表");
 			if($play_id = $this->player->shiftMusicList()){
 				$this->playId = $play_id;
 			}else{
-				echo '播放列表暂无歌曲'.PHP_EOL;
+				$this->serverLog("server.class->onplay 播放列表暂无歌曲");
 			}
 		}elseif($this->playTime < 1){
 			$this->player->getMp3($this->playId);
 			if(isset($this->player->timeList[$this->playId])){
 				$this->playTime = $this->player->timeList[$this->playId];
-				echo '已初始化播放曲目 '.$this->playId .' - '.$this->playTime.PHP_EOL;
-				$this->playBuffer = 0;
+				$this->serverLog('已初始化播放曲目 '.$this->playId .' - '.$this->playTime);
 			}else{
-				echo '正在初始化播放曲目 '.$this->playId .PHP_EOL;
+				$this->serverLog('正在初始化播放曲目 '.$this->playId);
 				$this->playTime = 0;
 			}
 		}else{
 			$this->playTime--;
-			echo '正在播放 剩余:'.$this->playTime.PHP_EOL;
+			if($this->playTime %10 ==0){
+				$this->serverLog('正在播放 剩余 '.$this->playTime);
+			}
+
 			if($this->playTime < 1){
 				$this->playId = 0;
-				echo '当前曲目播放完毕'.PHP_EOL;
+				$this->serverLog('当前曲目播放完毕 '.$this->playId);
 			}
 		}
 
+	}
 
+	function serverLog($msg){
+		$logPath = __DIR__ .'/../data/server.log';
+		if(method_exists($this,'stats')){
+			$stats = $this->server->stats();
+			$msgFormat = '['.date('Y-m-d H:i:s').'] '.$msg.
+				' Status: connect '.$stats['connection_num'].', accept '.$stats['accept_count'].
+				', close '.$stats['close_count'].', task '.$stats['tasking_num'] .
+				' Memusage'.$this->convert(memory_get_usage(true)) . PHP_EOL;
+		}else{
+			$msgFormat = '['.date('Y-m-d H:i:s').'] '.$msg.' Memusage'.$this->convert(memory_get_usage(true)) . PHP_EOL;
+		}
 
+		file_put_contents($logPath,$msgFormat,FILE_APPEND);
+		echo $msg.PHP_EOL;
+	}
+
+	private function convert($size)
+	{
+		$unit=array('b','kb','mb','gb','tb','pb');
+		return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
 	}
 
 	function start(){
--- music.php
+++ music.php
@@ -5,91 +5,12 @@
  * Date: 15-7-31
  * Time: 下午2:57
  */
+define('ROOT',__DIR__);
 require(__DIR__ . '/lib/server.class.php');
 require(__DIR__ . '/lib/player.class.php');
 require(__DIR__ . '/lib/mp3file.class.php');
-
-//$fp=fopen(__DIR__.'/data/2.mp3','rb');
-//$str=fread($fp,8096);
-//file_put_contents(__DIR__.'/data/1',$str);
-//fclose($fp);
-
-
-
-
-
-//$player = new player();
-//$url = '';
-//$xiamiUrl = 'http://www.xiami.com/song/playlist/id/1773571331';
-//print_r($player->getStream('http://m5.file.xiami.com/949/90949/1311688232/1773571331_15971564_l.mp3?auth_key=7016aabf1550cb0eccfa7222c934183e-1438732800-0-null',1024));
-//exit;
-//
-//$ret = $player->getCurl($xiamiUrl);
-//$argv = simplexml_load_string($ret, 'SimpleXMLElement', LIBXML_NOCDATA);
-//$mp3 = $player->de_Location($argv->trackList->track->location);
-//$player->getCurl($xiamiUrl,true);
-//
-//echo $mp3;
-//
-//exit;
-//$obj=new mp3file(__DIR__.'/data/1');
-//$meta=$obj->get_metadata();
-//print_r($obj->getframesize($meta));
-//exit;
-
-
-
-
-
-
-
-register_shutdown_function('handleFatal');
-function handleFatal()
-{
-	$error = error_get_last();
-	if (isset($error['type']))
-	{
-		switch ($error['type'])
-		{
-			case E_ERROR :
-			case E_PARSE :
-			case E_DEPRECATED:
-			case E_CORE_ERROR :
-			case E_COMPILE_ERROR :
-				$message = $error['message'];
-				$file = $error['file'];
-				$line = $error['line'];
-				$log = "$message ($file:$line)\nStack trace:\n";
-				$trace = debug_backtrace();
-				foreach ($trace as $i => $t)
-				{
-					if (!isset($t['file']))
-					{
-						$t['file'] = 'unknown';
-					}
-					if (!isset($t['line']))
-					{
-						$t['line'] = 0;
-					}
-					if (!isset($t['function']))
-					{
-						$t['function'] = 'unknown';
-					}
-					$log .= "#$i {$t['file']}({$t['line']}): ";
-					if (isset($t['object']) && is_object($t['object']))
-					{
-						$log .= get_class($t['object']) . '->';
-					}
-					$log .= "{$t['function']}()\n";
-				}
-				if (isset($_SERVER['REQUEST_URI']))
-				{
-					$log .= '[QUERY] ' . $_SERVER['REQUEST_URI'];
-				}
-				error_log($log);
-		}
-	}
-}
+require(__DIR__ . '/lib/event.class.php');
 
 $radio = new Server();
+$radio->init();
 $radio->start();
